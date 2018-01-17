5ccba9a2d70de1c228630cdf5074e6c6c73c551b
cmg019-1370483 <cmg019@uw.edu>
2018-01-16 16:12:14 -0800

    Debugging lab

--- exercise-wk2/exercise1.R
+++ exercise-wk2/exercise1.R
@@ -7,9 +7,11 @@ my.vector <- c(my.num, initials)
 
 # run ?sum to get more info
 vector.sum <- sum(my.vector)
+?sum
 
 # Describe why this doesn't work: 
-# 
+# In the sum funciton you cannot use words, which is why it says an invalid type (character)
+#of argument. Also my.vector does not work. 
 
 install.packages("stringr")
 
@@ -18,12 +20,12 @@ my.line <- "Hey, hey, this is the library"
 print(str_length(my.line))
 
 # Describe why this doesn't work: 
-# 
+# You need to do the library function after you install the package. 
 
 said.the.famous <- paste(my.line, " - ", initial)
 
 # Describe why this doesn't work: 
-# 
+#Initial was never made/established so calling it in the paste function doesnt do anything. 
 
 
 ## Part 2 - Vector and function practice
@@ -32,6 +34,14 @@ said.the.famous <- paste(my.line, " - ", initial)
 
 # Write a function `CompareLength` that takes in 2 vectors, and returns the sentence:
 # "The difference in lengths is N"
+CompareLength<- function(v1,v2) {
+  v.length<- abs(length(v1) - length(v2))
+  paste("The difference in lengths is", v.length)
+}
+v1<- c(1,2,3,4)
+v2<- c(1,2,3,4,5)
+
+CompareLength(v1,v2)
 
 
 # Pass two vectors of different length to your `CompareLength` function
@@ -40,6 +50,9 @@ said.the.famous <- paste(my.line, " - ", initial)
 # Write a function `DescribeDifference` that will return one of the following statements:
 # "Your first vector is longer by N elements"
 # "Your second vector is longer by N elements"
+DescribeDifference<- function(v1,v2) {
+  
+}
 
 # Pass two vectors to your `DescribeDifference` function
 
